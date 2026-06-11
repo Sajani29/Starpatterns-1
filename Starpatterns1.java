ad7df8c7ef35eb23c9c2df96fe49fa5b66245aa3
Sajani <sajani.siriwardhana29@gmail.com>
2026-06-12 00:54:26 +0530

    Second commit

--- Starpatterns1.java
+++ Starpatterns1.java
@@ -1,11 +1,22 @@
 import java.util.*;
 class Starpatterns1{
-	public static void main(String[] args){
-	for (int i = 0; i < 5; i++)
-	{
-		System.out.print("*");
+	
+	public static void printPattern(int count){
+		for (int i = 0; i < count; i++)
+		{
+		for (int j = 0; j < count; j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+		}
 	}
 	
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter count you need print squar pattern: ");
+		int count = input.nextInt();
+		printPattern(count);
 	}
 	
 }
